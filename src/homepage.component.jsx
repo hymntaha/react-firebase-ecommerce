71de67308c887882d80dab40e788f2d3702172ef
taha uygun <tahauygun@gmail.com>
2019-10-20 23:01:22 -0700

    setting up homepage

---
+++ src/homepage.component.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Homepage = () => (
+  <div className='homepage'>
+    <div className="directory-menu">
+      <div className="menu-item">
+        <div className="content">
+          <h1 className="title">
+            HATS
+            <span className="subtitle">SHOP NOW</span>
+          </h1>
+        </div>
+      </div>
+    </div>
+  </div>
+)
