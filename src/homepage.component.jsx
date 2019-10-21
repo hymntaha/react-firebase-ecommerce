7bb96327e6000237a91a93c542937edaa6446c1b
taha uygun <tahauygun@gmail.com>
2019-10-20 23:03:32 -0700

    homepage continues

--- src/homepage.component.jsx
+++ src/homepage.component.jsx
@@ -11,6 +11,40 @@ const Homepage = () => (
           </h1>
         </div>
       </div>
+      <div className="menu-item">
+        <div className="content">
+          <h1 className="title">
+            JACKETS
+            <span className="subtitle">SHOP NOW</span>
+          </h1>
+        </div>
+      </div>
+      <div className="menu-item">
+        <div className="content">
+          <h1 className="title">
+              SNEAKERS
+            <span className="subtitle">SHOP NOW</span>
+          </h1>
+        </div>
+      </div>
+      <div className="menu-item">
+        <div className="content">
+          <h1 className="title">
+            WOMENS
+            <span className="subtitle">SHOP NOW</span>
+          </h1>
+        </div>
+      </div>
+      <div className="menu-item">
+        <div className="content">
+          <h1 className="title">
+            MENS
+            <span className="subtitle">SHOP NOW</span>
+          </h1>
+        </div>
+      </div>
     </div>
   </div>
 )
+
+export default Homepage;
