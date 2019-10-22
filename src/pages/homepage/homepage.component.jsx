d89cbac4ef41714d9e1a3231caccfba1a7de3094
taha uygun <tahauygun@gmail.com>
2019-10-22 00:45:51 -0700

    dealing with style shit

--- src/pages/homepage/homepage.component.jsx
+++ src/pages/homepage/homepage.component.jsx
@@ -1,49 +1,10 @@
 import React from 'react';
+import Directory from "../../components/directory/directory.component";
+import './homepage.style.scss';
 
 const Homepage = () => (
   <div className='homepage'>
-    <div className="directory-menu">
-      <div className="menu-item">
-        <div className="content">
-          <h1 className="title">
-            HATS
-            <span className="subtitle">SHOP NOW</span>
-          </h1>
-        </div>
-      </div>
-      <div className="menu-item">
-        <div className="content">
-          <h1 className="title">
-            JACKETS
-            <span className="subtitle">SHOP NOW</span>
-          </h1>
-        </div>
-      </div>
-      <div className="menu-item">
-        <div className="content">
-          <h1 className="title">
-              SNEAKERS
-            <span className="subtitle">SHOP NOW</span>
-          </h1>
-        </div>
-      </div>
-      <div className="menu-item">
-        <div className="content">
-          <h1 className="title">
-            WOMENS
-            <span className="subtitle">SHOP NOW</span>
-          </h1>
-        </div>
-      </div>
-      <div className="menu-item">
-        <div className="content">
-          <h1 className="title">
-            MENS
-            <span className="subtitle">SHOP NOW</span>
-          </h1>
-        </div>
-      </div>
-    </div>
+    <Directory/>
   </div>
 )
 
