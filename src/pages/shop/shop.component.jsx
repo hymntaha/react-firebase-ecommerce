c7c60529c9dde41f34349f740831d58dcbc59f8d
taha uygun <tahauygun@gmail.com>
2019-10-23 21:42:07 -0700

    fixing shop preview

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -1,5 +1,7 @@
 import React, {Component} from 'react';
 import SHOP_DATA from "./shop.data";
+import CollectionPreview from '../../components/collection-preview/collection-preview.component'
+
 
 class ShopComponent extends Component {
   constructor(props) {
@@ -10,9 +12,14 @@ class ShopComponent extends Component {
     }
   }
   render() {
+    const {collections} = this.state;
     return (
-      <div>
-        SHOP PAGE
+      <div className='shop-page'>
+        {
+          collections.map({(id, ...otherCollectionProps})=>{
+            <CollectionPreview key={id} {...otherCollectionsProps}/>
+        })
+        }
       </div>
     );
   }
