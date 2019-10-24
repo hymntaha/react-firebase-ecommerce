505abd3c7dc04c5a534a344ae6c0087836370503
taha uygun <tahauygun@gmail.com>
2019-10-23 21:28:58 -0700

    working page

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -1,19 +1,18 @@
 import React, {Component} from 'react';
+import SHOP_DATA from "./shop.data";
 
 class ShopComponent extends Component {
   constructor(props) {
     super(props);
 
     this.state = {
-      collections: [
-
-      ]
+      collections: SHOP_DATA
     }
   }
   render() {
     return (
       <div>
-        
+        SHOP PAGE
       </div>
     );
   }
