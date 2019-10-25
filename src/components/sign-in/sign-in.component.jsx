0b31555f6c1aa0e86f03d28ccb0b1edae296e8a1
taha uygun <tahauygun@gmail.com>
2019-10-25 10:30:40 -0700

    adding signin page

--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -10,14 +10,25 @@ class SignInComponent extends Component {
       password: ""
     };
   }
+
+  handleSubmit = (e) =>
+  {
+    e.preventDefault();
+    this.setState({email:'', password:''})
+  }
+
   render() {
     return (
       <div className="sign-in">
         <h2>I already have an account</h2>
         <span>Sign in with your email and password</span>
 
-        <form>
-          <input name="email" />
+        <form onSubmit={this.handleSubmit}>
+          <label htmlFor="email">Email</label>
+          <input name="email" value={this.state.email} required/>
+          <label htmlFor="password">Password</label>
+          <input name="password" value={this.state.password} required/>
+          <input type='submut' value='Submit Form'/>
         </form>
       </div>
     );
