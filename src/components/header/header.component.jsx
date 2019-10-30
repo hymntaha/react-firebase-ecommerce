2346a61a94bcb7891ddc68fa6e8a5184e8abccc2
taha uygun <tahauygun@gmail.com>
2019-10-29 22:15:50 -0700

    adding signout func

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -1,9 +1,12 @@
 import React from 'react';
 import {Link} from 'react-router-dom';
 import {ReactComponent as Logo } from "../../assets/crown.svg";
+
+import {auth} from "../../firebase/app/firebase.utils";
+
 import './header.styles.scss';
 
-const Header = () => (
+const Header = ({currentUser}) => (
   <div className='header'>
     <Link className='logo-container' to="/">
       <Logo className='logo'/>
@@ -15,6 +18,14 @@ const Header = () => (
       <Link className='option' to='/contact'>
         CONTACT
       </Link>
+      {
+        currentUser ?
+          <div></div>
+          :
+          <Link>
+
+          </Link>
+      }
     </div>
   </div>
 )
