23facb80e945dc274ee5cf9c38e8832f139c73bf
taha uygun <tahauygun@gmail.com>
2019-10-29 22:13:15 -0700

    continue unsubscribeFromAuth

--- src/App.js
+++ src/App.js
@@ -18,12 +18,18 @@ class App extends Component {
     }
   }
 
+  unsubscribeFromAuth = null
+
   componentDidMount() {
-    auth.onAuthStateChanged(user=> {
+    this.unsubscribeFromAuth = auth.onAuthStateChanged(user=> {
       this.setState({
         currentUser: user
       })
     })
+  }
+
+  componentWillUnmount() {
+    this.unsubscribeFromAuth();
   }
 
   render() {
