0c4c3720e2b800e34a045ea8414d28434db5f7c6
taha uygun <tahauygun@gmail.com>
2019-10-30 22:32:15 -0700

    adding firestore

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import Homepage from "./pages/homepage/homepage.component";
 import ShopPage from "./pages/shop/shop.component";
 import SignInAndSignUpPage from "./pages/sign-in-and-sign-up/sign-in-and-sign-up.component";
 import Header from "./components/header/header.component";
-import { auth } from "./firebase/app/firebase.utils";
+import { auth, createUserProfileDocument } from "./firebase/app/firebase.utils";
 
 class App extends Component {
   constructor(){
@@ -21,10 +21,8 @@ class App extends Component {
   unsubscribeFromAuth = null
 
   componentDidMount() {
-    this.unsubscribeFromAuth = auth.onAuthStateChanged(user=> {
-      this.setState({
-        currentUser: user
-      })
+    this.unsubscribeFromAuth = auth.onAuthStateChanged(async user=> {
+      createUserProfileDocument(user);
     })
   }
 
