d3d5678fd76b04827e6cc99f1b563f45640b244f
taha uygun <tahauygun@gmail.com>
2019-10-30 22:38:12 -0700

    storing user data

--- src/App.js
+++ src/App.js
@@ -21,8 +21,14 @@ class App extends Component {
   unsubscribeFromAuth = null
 
   componentDidMount() {
-    this.unsubscribeFromAuth = auth.onAuthStateChanged(async user=> {
-      createUserProfileDocument(user);
+    this.unsubscribeFromAuth = auth.onAuthStateChanged(async userAuth=> {
+      if (userAuth) {
+        const userRef = await createUserProfileDocument(userAuth);
+
+        userRef.onSnapshot(snapshot => {
+
+        })
+      }
     })
   }
 
