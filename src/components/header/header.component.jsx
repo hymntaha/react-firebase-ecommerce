eb490b59df8279ae78e06cb4ade4dd9422e5f3d4
taha uygun <tahauygun@gmail.com>
2019-11-06 21:06:55 -0800

    cart component

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import {Link} from 'react-router-dom';
 import {connect} from 'react-redux';
 import {ReactComponent as Logo } from "../../assets/crown.svg";
-
+import CartIcon from "../cart-icon/cart-icon.component";
 import {auth} from "../../firebase/app/firebase.utils";
 
 import './header.styles.scss';
@@ -27,6 +27,7 @@ const Header = ({currentUser}) => (
             SIGN IN
           </Link>
       }
+      <CartIcon/>
     </div>
   </div>
 )
