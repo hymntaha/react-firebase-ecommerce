527a1d95c13e1b00ee612e50c101c61a422f5308
taha uygun <tahauygun@gmail.com>
2019-11-08 23:22:10 -0800

    cart item reducer

--- src/redux/cart/cart.actions.js
+++ src/redux/cart/cart.actions.js
@@ -3,3 +3,8 @@ import CartACtionTypes from "./cart.types";
 export const toggleCartHidden = () => ({
   type: CartACtionTypes.TOGGLE_CART_HIDDEN
 })
+
+export const addItem = item => ({
+  type: CartACtionTypes.ADD_ITEM,
+  payload: item
+})
