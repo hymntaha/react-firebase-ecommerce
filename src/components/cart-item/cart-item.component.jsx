f9e23860bce4d50c07ccfa76b635eedc23a1ea03
taha uygun <tahauygun@gmail.com>
2019-11-10 23:34:33 -0800

    item component in cart dropdown

--- src/components/cart-item/cart-item.component.jsx
+++ src/components/cart-item/cart-item.component.jsx
@@ -2,10 +2,13 @@ import React from 'react';
 import './cart-item.styles.scss';
 
 
-const CartItem = ({item: {imageUrl, price, name}}) => (
+const CartItem = ({item: {imageUrl, price, name,quantity}}) => (
   <div className="cart-item">
     <img src={imageUrl} alt=""/>
-
+    <div className="item-details">
+      <span className="name">{name}</span>
+      <span className="price"> {quantity} X {price}</span>
+    </div>
   </div>
 );
 
