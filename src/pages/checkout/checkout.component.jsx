707f9169d0d23e8a4e8ec4379443b8e3cefa561d
taha uygun <tahauygun@gmail.com>
2019-11-13 20:37:14 -0800

    adding total prop to checkout page

--- src/pages/checkout/checkout.component.jsx
+++ src/pages/checkout/checkout.component.jsx
@@ -1,6 +1,9 @@
 import React from 'react';
+import {connect} from 'react-redux';
+import {createStructuredSelector} from "reselect";
+import {selectCartItems, selectCartTotal} from "../../redux/cart/cart.selectors";
 
-const CheckoutPage = () => {
+const CheckoutPage = ({cartItems, total}) => {
   return (
    <div className="checkout-page">
      <div className="checkout-header">
@@ -20,8 +23,19 @@ const CheckoutPage = () => {
          <span>Remove</span>
        </div>
      </div>
+     {
+       cartItems.map(cartItem=> cartItem.name)
+     }
+     <div className="total">
+       <span>TOTAL: ${total}</span>
+     </div>
    </div>
   );
 };
 
-export default CheckoutPage;
+const mapStateToProps = createStructuredSelector({
+  cartItems: selectCartItems,
+  total: selectCartTotal
+})
+
+export default connect(mapStateToProps)(CheckoutPage);
