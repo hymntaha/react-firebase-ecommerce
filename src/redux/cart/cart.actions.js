b1457d02d77322a937de9c50e53c8ac1aee8f2bf
taha uygun <tahauygun@gmail.com>
2019-11-15 17:55:21 -0800

    adding remove action and reducer for cart

--- src/redux/cart/cart.actions.js
+++ src/redux/cart/cart.actions.js
@@ -8,3 +8,8 @@ export const addItem = item => ({
   type: CartACtionTypes.ADD_ITEM,
   payload: item
 })
+
+export const clearItemFromCart = item=>({
+  type: CartACtionTypes.CLEAR_ITEM_FROM_CART,
+  payload: item
+})
