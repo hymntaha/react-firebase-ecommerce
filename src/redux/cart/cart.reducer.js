f8fe97b85cda14172787d9f9d4eb392f1039540a
taha uygun <tahauygun@gmail.com>
2019-11-16 19:14:26 -0800

    reducer setup for cart item remove

--- src/redux/cart/cart.reducer.js
+++ src/redux/cart/cart.reducer.js
@@ -1,5 +1,5 @@
 import CartActionTypes from './cart.types'
-import {addItemToCart} from "./cart.utils";
+import {addItemToCart, removeItemFromCart} from "./cart.utils";
 
 const INITIAL_STATE = {
   hidden:true,
@@ -21,7 +21,7 @@ const cartReducer = (state=INITIAL_STATE, action)=> {
     case CartActionTypes.REMOVE_ITEM:
       return{
         ...state,
-        cartItems:addItemToCart(state.cartItems, action.payload)
+        cartItems:removeItemFromCart(state.cartItems, action.payload)
       }
     case CartActionTypes.CLEAR_ITEM_FROM_CART:
       return{
