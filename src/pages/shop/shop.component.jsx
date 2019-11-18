a8bdd186e02f4099d69e178c611554282f0fd968
taha uygun <tahauygun@gmail.com>
2019-11-17 22:40:39 -0800

    finishing up shop refactoring

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -1,5 +1,9 @@
 import React, { Component } from "react";
+import {connect} from 'react-redux';
+import {createStructuredSelector} from "reselect";
+
 import CollectionPreview from "../../components/collection-preview/collection-preview.component";
+import {selectCollections} from "../../redux/shop/shop.selector";
 
 const ShopComponent = ({ collections }) => (
   <div className="shop-page">
@@ -9,4 +13,9 @@ const ShopComponent = ({ collections }) => (
   </div>
 );
 
-export default ShopComponent;
+const mapStateToProps = createStructuredSelector({
+  collections: selectCollections
+})
+
+
+export default connect(mapStateToProps)(ShopComponent);
