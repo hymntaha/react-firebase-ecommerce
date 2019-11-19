286dc06a1d8dcc04b81c0025abe47175dcd65038
taha uygun <tahauygun@gmail.com>
2019-11-18 22:38:54 -0800

    adding category page

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -5,7 +5,7 @@ import {Route} from 'react-router-dom';
 
 const ShopComponent = ({match}) => (
   <div className="shop-page">
-    <Route exact path={} component={CollectionOverview}/>
+    <Route exact path={`${match.path}`} component={CollectionOverview}/>
   </div>
 );
 
