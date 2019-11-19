1b02c145e6e21b60db4742c1f4993594ad0d6874
taha uygun <tahauygun@gmail.com>
2019-11-18 22:33:57 -0800

    styling collection overview

--- src/components/collection-overview/collection-overview.component.jsx
+++ src/components/collection-overview/collection-overview.component.jsx
@@ -1,14 +1,23 @@
 import React from 'react';
 import {connect} from 'react-redux';
 import {createStructuredSelector} from "reselect";
+import CollectionPreview from "../collection-preview/collection-preview.component";
+import {selectCollections} from "../../redux/shop/shop.selector";
 
 import './collections-overview.styles.scss'
-const CollectionsOverview = () => {
-  return (
-    <div>
 
-    </div>
-  );
-};
+const CollectionsOverview = ({collections}) => (
+  <div className="collections-overview">
+    {collections.map(({ id, ...otherCollectionProps }) => (
+      <CollectionPreview key={id} {...otherCollectionProps} />
+    ))}
+  </div>
+)
 
-export default CollectionsOverview;
+
+
+const mapStateToProps = createStructuredSelector({
+  collections: selectCollections
+})
+
+export default connect(mapStateToProps)(CollectionsOverview);
--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -1,21 +1,10 @@
 import React, { Component } from "react";
-import {connect} from 'react-redux';
-import {createStructuredSelector} from "reselect";
-
-import CollectionPreview from "../../components/collection-preview/collection-preview.component";
-import {selectCollections} from "../../redux/shop/shop.selector";
+import CollectionOverview from "../../components/collection-overview/collection-overview.component";
 
 const ShopComponent = ({ collections }) => (
   <div className="shop-page">
-    {collections.map(({ id, ...otherCollectionProps }) => (
-      <CollectionPreview key={id} {...otherCollectionProps} />
-    ))}
+    <CollectionOverview />
   </div>
 );
 
-const mapStateToProps = createStructuredSelector({
-  collections: selectCollections
-})
-
-
-export default connect(mapStateToProps)(ShopComponent);
+export default ShopComponent;
