a111ff302ab67f041d77264b0f011292ad490cbc
taha uygun <tahauygun@gmail.com>
2019-11-18 22:26:07 -0800

    separating collection from shop

---
+++ src/components/collection-overview/collection-overview.component.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import {connect} from 'react-redux';
+import {createStructuredSelector} from "reselect";
+
+import './collections-overview.styles.scss'
+const CollectionsOverview = () => {
+  return (
+    <div>
+
+    </div>
+  );
+};
+
+export default CollectionsOverview;
