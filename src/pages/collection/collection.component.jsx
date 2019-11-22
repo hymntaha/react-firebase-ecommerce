bb73b968be33b3d4079095c278345f1c0f9e71f5
taha uygun <tahauygun@gmail.com>
2019-11-21 21:20:55 -0800

    adding props to collection page

--- src/pages/collection/collection.component.jsx
+++ src/pages/collection/collection.component.jsx
@@ -6,9 +6,16 @@ import './collection.styles.scss';
 import {selectCollection} from "../../redux/shop/shop.selector";
 
 const CollectionPage = ({collection}) => {
+  const {title, items} = collection;
+
   return (
     <div className='collection-page'>
-      <h2>Category Page</h2>
+      <h2 className='title'>{title}</h2>
+      <div className="items">
+        {
+          items.map(item=>(<CollectionItem key={item.id} item={item}/>))
+        }
+      </div>
     </div>
   );
 };
