37dd386d134c7f6b26d01a615b61b8371cd624d2
taha uygun <tahauygun@gmail.com>
2019-11-23 23:49:20 -0800

    setting up stripe checkout button

---
+++ src/components/stripe-button/stripe-button.component.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import StripeCheckout from "react-stripe-checkout";
+
+const StripeCheckoutButton = ({price}) => {
+  return (
+    <div>
+      
+    </div>
+  );
+};
+
+export default StripeCheckoutButton;
