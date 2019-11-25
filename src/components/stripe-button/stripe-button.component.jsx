da8c5dbdab756888b8c2db8499427f554c31b6b4
taha uygun <tahauygun@gmail.com>
2019-11-24 23:38:14 -0800

    adding stripe button to checkout comp

--- src/components/stripe-button/stripe-button.component.jsx
+++ src/components/stripe-button/stripe-button.component.jsx
@@ -1,13 +1,27 @@
-import React from 'react';
+import React from "react";
 import StripeCheckout from "react-stripe-checkout";
 
-const StripeCheckoutButton = ({price}) => {
+const StripeCheckoutButton = ({ price }) => {
   const priceForStripe = price * 100;
-  const publishableKey = 'THERE WILL BE A STRIPE KEY HERE'
+  const publishableKey = "THERE WILL BE A STRIPE KEY HERE";
+
+  const onToken = token =>{
+    console.log(token)
+  }
 
   return (
-    <StripeCheckout/>
-  )
+    <StripeCheckout
+      label="Pay Now"
+      name="CRWN Clothing Ltd."
+      billingAddress
+      shippingAddress
+      image="https://svgshare.com/i/CUz.svg"
+      amount={priceForStripe}
+      panelLabel='Pay Now'
+      token={onToken}
+      stripeKey={publishableKey}
+    />
+  );
 };
 
 export default StripeCheckoutButton;
