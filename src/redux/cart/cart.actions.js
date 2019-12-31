7c9907f5fb278089d93f4f49076237ec8c18f007
taha uygun <tahauygun@gmail.com>
2019-12-30 23:38:45 -0800

    some modifications

--- src/redux/cart/cart.actions.js
+++ src/redux/cart/cart.actions.js
@@ -1,20 +1,10 @@
-import CartACtionTypes from "./cart.types";
+import CartActionTypes from './cart.types';
 
 export const toggleCartHidden = () => ({
-  type: CartACtionTypes.TOGGLE_CART_HIDDEN
-})
+  type: CartActionTypes.TOGGLE_CART_HIDDEN
+});
 
 export const addItem = item => ({
-  type: CartACtionTypes.ADD_ITEM,
+  type: CartActionTypes.ADD_ITEM,
   payload: item
-})
-
-export const removeItem = item => ({
-  type: CartACtionTypes.REMOVE_ITEM,
-  payload: item
-})
-
-export const clearItemFromCart = item=>({
-  type: CartACtionTypes.CLEAR_ITEM_FROM_CART,
-  payload: item
-})
+});
--- src/redux/cart/cart.types.js
+++ src/redux/cart/cart.types.js
@@ -1,8 +1,6 @@
-const CartACtionTypes = {
+const CartActionTypes = {
   TOGGLE_CART_HIDDEN: 'TOGGLE_CART_HIDDEN',
-  ADD_ITEM: 'ADD_ITEM',
-  REMOVE_ITEM: 'REMOVE_ITEM',
-  CLEAR_ITEM_FROM_CART:'CLEAR_ITEM_FROM_CART'
-}
+  ADD_ITEM: 'ADD_ITEM'
+};
 
-export default CartACtionTypes;
+export default CartActionTypes;
