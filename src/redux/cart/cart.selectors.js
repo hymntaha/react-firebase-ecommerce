78daed4af4518898370b4126b1e9e7d3c518f06e
taha uygun <tahauygun@gmail.com>
2019-12-31 16:17:30 -0800

    handling hoc in redux

--- src/redux/cart/cart.selectors.js
+++ src/redux/cart/cart.selectors.js
@@ -1,4 +1,4 @@
-import { createSelector } from "reselect";
+import { createSelector } from 'reselect';
 
 const selectCart = state => state.cart;
 
@@ -9,14 +9,15 @@ export const selectCartItems = createSelector(
 
 export const selectCartHidden = createSelector(
   [selectCart],
-  cart=> cart.hidden
+  cart => cart.hidden
 );
 
 export const selectCartItemsCount = createSelector(
   [selectCartItems],
   cartItems =>
     cartItems.reduce(
-      (accumlatedQuantity, cartItem) => accumlatedQuantity + cartItem.quantity,
+      (accumalatedQuantity, cartItem) =>
+        accumalatedQuantity + cartItem.quantity,
       0
     )
 );
@@ -25,7 +26,8 @@ export const selectCartTotal = createSelector(
   [selectCartItems],
   cartItems =>
     cartItems.reduce(
-      (accumlatedQuantity, cartItem) => accumlatedQuantity + cartItem.quantity  * cartItem.price,
+      (accumalatedQuantity, cartItem) =>
+        accumalatedQuantity + cartItem.quantity * cartItem.price,
       0
     )
-)
+);
--- src/redux/directory/directory.reducer.js
+++ src/redux/directory/directory.reducer.js
@@ -1,10 +1,11 @@
 const INITIAL_STATE = {
-  sections: [ {
-    title: 'hats',
-    imageUrl: 'https://i.ibb.co/cvpntL1/hats.png',
-    id: 1,
-    linkUrl: 'shop/hats'
-  },
+  sections: [
+    {
+      title: 'hats',
+      imageUrl: 'https://i.ibb.co/cvpntL1/hats.png',
+      id: 1,
+      linkUrl: 'shop/hats'
+    },
     {
       title: 'jackets',
       imageUrl: 'https://i.ibb.co/px2tCc3/jackets.png',
@@ -30,14 +31,15 @@ const INITIAL_STATE = {
       size: 'large',
       id: 5,
       linkUrl: 'shop/mens'
-    }]
-}
+    }
+  ]
+};
 
-const directoryReducer = ( state = INITIAL_STATE, action) => {
+const directoryReducer = (state = INITIAL_STATE, action) => {
   switch (action.type) {
     default:
       return state;
   }
-}
+};
 
 export default directoryReducer;
